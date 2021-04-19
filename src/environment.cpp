55e8ade69d487e53ecee43b48a553770fd331267
thomasdunlap <tom.a.dunlap@gmail.com>
2021-04-19 08:48:10 -0700

    Updated environment.cpp and lidar.h to view lidar as particle zones instead of just rays.

--- src/environment.cpp
+++ src/environment.cpp
@@ -42,13 +42,18 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     // ----------------------------------------------------
     
     // RENDER OPTIONS
-    bool renderScene = true;
+    bool renderScene = false;
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
-    // TODO:: Create lidar sensor 
+    // TODO:: Create lidar sensor
+    Lidar* lidar = new Lidar(cars, 0); 
 
     // TODO:: Create point processor
-  
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud = lidar->scan();
+    //renderRays(viewer, lidar->position, cloud);
+    Color c(.5, 0, 0);
+    renderPointCloud(viewer, cloud, "Tom is great!", c);
+
 }
 
 
